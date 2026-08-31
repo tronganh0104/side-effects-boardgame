7871d429785100e21c66ff3075a6422bdb884eee
Girzmo2610 <hoangtuantu893@gmail.com>
2026-08-31 17:10:13 +0700

    Check

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,6 @@
-import { defineConfig } from 'vitest/config'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  test: {
-    include: ['src/**/tests/**/*.test.ts', 'server/tests/**/*.test.ts'],
-  },
 })
